c45a97c1cb55da742091a979e3f901637307ed4e
Ad <103935216+adzhl@users.noreply.github.com>
2024-10-30 09:35:39 +0800

    Added builtin_pwd.c

---
+++ src/builtin/builtin_pwd.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtin_pwd.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abinti-a <abinti-a@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/29 14:06:37 by abinti-a          #+#    #+#             */
+/*   Updated: 2024/10/30 09:29:23 by abinti-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/builtin.h"
+
+int	builtin_pwd(char **args, char **env)
+{
+	char	cwd[PATH_MAX];
+
+	(void)args;
+	(void)env;
+	if (!getcwd(cwd, sizeof(cwd)))
+	{
+		perror("pwd");
+		return (1);
+	}
+	ft_putendl_fd(cwd, STDOUT_FILENO);
+	return (0);
+}
+
+/* int main(void)
+{
+    if (builtin_pwd(NULL, NULL) != 0)
+    {
+        printf("Error\n");
+        return (1);
+    }
+    return (0);
+} */
