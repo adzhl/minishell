353dd5db5daea1b02aa8f9597733ef122b734412
Ad <103935216+adzhl@users.noreply.github.com>
2024-10-31 14:10:10 +0800

    Removed global variable from builtin.h and builtin.c

--- include/builtin.h
+++ include/builtin.h
@@ -6,7 +6,7 @@
 /*   By: abinti-a <abinti-a@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 09:09:39 by abinti-a          #+#    #+#             */
-/*   Updated: 2024/10/29 14:00:23 by abinti-a         ###   ########.fr       */
+/*   Updated: 2024/10/31 13:48:13 by abinti-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 # include <unistd.h>
 # include <stdlib.h>
+# include <limits.h>
 # include "../libft/ft_printf/includes/ft_printf.h"
 # include "../libft/get_next_line.h"
 # include "../libft/libft.h"
@@ -23,33 +24,30 @@
  * Built-in command function pointer type
  * Returns: Exit status of the command (0 for success, other for failure)
  */
-typedef int (*t_builtin_func)(char **args, char **env);
+typedef int	(*t_builtin_func)(char **args, char **env);
 
 /**
  * name = cmd name
  * func = pointer to the function that implements the cmd
  */
-typedef struct  s_builtin
+typedef struct s_builtin
 {
-    char *name;
-    t_builtin_func func;
-}               t_builtin;
-
-//Global built-ins table
-extern t_builtin g_builtins[];
+	char		*name;
+	t_builtin_func	func;
+}		t_builtin;
 
 //Built-in functions
-int builtin_echo(char **args, char **env);
-int builtin_cd(char **args, char **env);
-int builtin_pwd(char **args, char **env);
-int builtin_export(char **args, char **env);
-int builtin_unset(char **args, char **env);
-int builtin_env(char **args, char **env);
-int builtin_exit(char **args, char **env);
+int		builtin_echo(char **args, char **env);
+int		builtin_cd(char **args, char **env);
+int		builtin_pwd(char **args, char **env);
+int		builtin_export(char **args, char **env);
+int		builtin_unset(char **args, char **env);
+int		builtin_env(char **args, char **env);
+int		builtin_exit(char **args, char **env);
 
 //Built-in utils
-//t_builtin *init_builtins(void);
-int is_builtin(char *cmd);
-int execute_builtin(char *cmd, char **args, char **env);
+t_builtin	*init_builtins(void);
+int		is_builtin(char *cmd);
+int		execute_builtin(char *cmd, char **args, char **env);
 
 #endif 
--- src/builtin/builtin.c
+++ src/builtin/builtin.c
@@ -6,7 +6,7 @@
 /*   By: abinti-a <abinti-a@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 11:28:56 by abinti-a          #+#    #+#             */
-/*   Updated: 2024/10/29 14:18:33 by abinti-a         ###   ########.fr       */
+/*   Updated: 2024/10/31 13:48:02 by abinti-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,68 +16,56 @@
  * Initialize built-in commands table 
  * A library that stores a list of built-in commands
  */
-/* t_builtin *init_builtin(void)
+t_builtin	*init_builtin(void)
 {
-    static t_builtin builtins[] =
-    {
-        {"echo", builtin_echo},
-        {"cd", builtin_cd},
-        {"pwd", builtin_pwd},
-        {"export", builtin_export},
-        {"unset", builtin_unset},
-        {"env", builtin_env},
-        {"exit", builtin_exit},
-        {NULL, NULL}
-    };
-    return (builtins);
-} */
-
-/**
- * Global built-in commands table 
- * A library that stores a list of built-in commands
- */
-t_builtin g_builtins[] =
-{
-    {"echo", builtin_echo},
-    {"cd", builtin_cd},
-    {"pwd", builtin_pwd},
-    {"export", builtin_export},
-    {"unset", builtin_unset},
-    {"env", builtin_env},
-    {"exit", builtin_exit},
-    {NULL, NULL}
-};
+	static	t_builtin builtins[] =
+	{
+		{"echo", builtin_echo},
+		{"cd", builtin_cd},
+		{"pwd", builtin_pwd},
+		{"export", builtin_export},
+		{"unset", builtin_unset},
+		{"env", builtin_env},
+		{"exit", builtin_exit},
+		{NULL, NULL}
+	};
+	return (builtins);
+}
 
 /**
  * Check if cmd is a built-in
  */
-int is_builtin(char *cmd)
+int	is_builtin(char *cmd)
 {
-    int i;
+	t_builtin	*builtins;
+	int		i;
 
-    i = 0;
-    while (g_builtins[i].name)
-    {
-        if (!ft_strcmp(cmd, g_builtins[i].name))
-            return (1);
-        i++;
-    }
-    return (0);
+	i = 0;
+	builtins = init_builtin();
+	while (builtins[i].name)
+	{
+		if (!ft_strcmp(cmd, builtins[i].name))
+			return (1);
+		i++;
+	}
+	return (0);
 }
 
 /**
  * Execute built-in commands
  */
-int execute_builtin(char *cmd, char **args, char **env)
+int	execute_builtin(char *cmd, char **args, char **env)
 {
-    int i;
+	t_builtin	*builtins;
+	int		i;
 
-    i = 0;
-    while (g_builtins[i].name)
-    {
-        if (!ft_strcmp(cmd, g_builtins[i].name))
-            return (g_builtins[i].func(args, env));
-        i++;
-    }
-    return (1);
+	i = 0;
+	builtins = init_builtin();
+	while (builtins[i].name)
+	{
+		if (!ft_strcmp(cmd, builtins[i].name))
+			return (builtins[i].func(args, env));
+		i++;
+	}
+	return (1);
 }
