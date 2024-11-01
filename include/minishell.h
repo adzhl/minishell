c84f442404c64671d22786a2a88d0a19ca8d0270
et-learns-to-code <155518416+et-learns-to-code@users.noreply.github.com>
2024-11-01 16:27:29 +0800

    coded basic expansion functions. test the functions and make sure the output is correct.

--- include/minishell.h
+++ include/minishell.h
@@ -6,18 +6,21 @@
 /*   By: etien <etien@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 13:47:31 by etien             #+#    #+#             */
-/*   Updated: 2024/11/01 11:29:48 by etien            ###   ########.fr       */
+/*   Updated: 2024/11/01 16:24:07 by etien            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-// program header files
-# include "parser.h"
-
 // libft library
 # include "../libft/libft/libft.h"
+
+// readline library
+// readline
+# include <readline/readline.h>
+// add_history
+# include <readline/history.h>
 
 // printf, perror
 # include <stdio.h>
@@ -34,9 +37,11 @@
 // open, close
 # include <fcntl.h>
 
-// readline
-# include <readline/readline.h>
-// add_history
-# include <readline/history.h>
+// boolean data type
+# include <stdbool.h>
+
+// program header files
+# include "expansion.h"
+# include "parser.h"
 
 #endif
