97d0d01dd6497a2431d06aaecdf6bee973d2a8c4
Adya Zahila Hasman Binti Aidi Hasman <abinti-a@u90z03s08.42kl.edu.my>
2024-11-06 10:55:56 +0800

    Modified builtin_cd.c to comply with norm

--- src/builtin/builtin_cd.c
+++ src/builtin/builtin_cd.c
@@ -6,7 +6,7 @@
 /*   By: abinti-a <abinti-a@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 14:57:06 by abinti-a          #+#    #+#             */
-/*   Updated: 2024/11/06 10:52:49 by abinti-a         ###   ########.fr       */
+/*   Updated: 2024/11/06 10:54:30 by abinti-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,24 +25,24 @@ void	print_cd_error(char *arg, char *message)
 
 int	builtin_cd(char **args, char **env)
 {
-    char *path;
+	char	*path;
 
-    (void)env;
-    if (!args[1])
-    {
-        path = getenv("HOME");
-        if (!path)
-        {
-            print_cd_error(NULL, "HOME not set");
-            return (1);
-        }
-    }
-    else
-        path = args[1];
-    if (chdir(path) == -1)
-    {
-        print_cd_error(args[1], "No such file or directory");
-        return (1);
-    }
-    return (0);
+	(void)env;
+	if (!args[1])
+	{
+		path = getenv("HOME");
+		if (!path)
+		{
+			print_cd_error(NULL, "HOME not set");
+			return (1);
+		}
+	}
+	else
+		path = args[1];
+	if (chdir(path) == -1)
+	{
+		print_cd_error(args[1], "No such file or directory");
+		return (1);
+	}
+	return (0);
 }
