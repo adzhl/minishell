ebd1a2f02fd8afa09ad5ed47e1b177e23d3a0814
et-learns-to-code <155518416+et-learns-to-code@users.noreply.github.com>
2024-11-06 15:29:15 +0800

    refactored expansion code. now complying with norm.

--- include/expansion.h
+++ include/expansion.h
@@ -6,7 +6,7 @@
 /*   By: etien <etien@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 14:16:29 by etien             #+#    #+#             */
-/*   Updated: 2024/11/05 10:35:37 by etien            ###   ########.fr       */
+/*   Updated: 2024/11/06 15:27:46 by etien            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,13 @@
 # define UNCLOSED_QUOTES "Syntax error: Unclosed quotes."
 
 char	*expand_var(char *s);
+void	expansion_control(char **s, char *initial_quote,
+			int *in_quote_context, char **expanded_s);
+
+// Util functions
 char	*sub_in_var(char **s, char *expanded_s);
+char	*append_expansion(char *expanded_s, char *expansion);
+char	*append_exit_status(char **s, char *expanded_s);
 char	*append_str(char **s, char *expanded_s, char initial_quote);
 
 #endif
