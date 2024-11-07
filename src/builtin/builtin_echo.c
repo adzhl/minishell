fba67f2dd4ff841f0aa796b17fef080986392b28
Ad <103935216+adzhl@users.noreply.github.com>
2024-11-07 16:30:43 +0800

    Modified builtin_echo.c to handle multiple -n options and -nnnn option

--- src/builtin/builtin_echo.c
+++ src/builtin/builtin_echo.c
@@ -6,12 +6,44 @@
 /*   By: abinti-a <abinti-a@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 12:12:16 by abinti-a          #+#    #+#             */
-/*   Updated: 2024/10/31 12:43:29 by abinti-a         ###   ########.fr       */
+/*   Updated: 2024/11/07 16:28:52 by abinti-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/builtin.h"
 
+/**
+ * Handle the case for:
+ * 
+ * Single -n
+ * Multiple -n
+ * -nnnnnn
+ * 
+ * Exclude: -nWord
+ */
+static int handle_n_option(char **args, int *newline)
+{
+    int i;
+    int j;
+
+    i = 1;
+    *newline = 1;
+    while (args[i])
+    {
+        if (args[i][0] != '-')
+            break;
+        if (args[i][1] != 'n')
+            break;
+        j = 2;
+        while (args[i][j] == 'n')
+            j++;
+        if (args[i][j] != '\0')
+            break;
+        *newline = 0;
+        i++;
+    }
+    return (i);
+}
 /**
  * Function prints out the string after echo is called
  * adds '\n' at the end of the last argument by default
@@ -24,22 +56,16 @@ int	builtin_echo(char **args, char **env)
 	int	newline;
 
 	(void)env;
-	i = 1;
-	newline = 1;
-	if (args[1] && ft_strcmp(args[1], "-n") == 0)
-	{
-		newline = 0;
-		i = 2;
-	}
+	i = handle_n_option(args, &newline);
 	while (args[i])
 	{
-		ft_printf("%s", args[i]);
+		printf("%s", args[i]);
 		if (args[i + 1])
-			ft_printf(" ");
+			printf(" ");
 		i++;
 	}
 	if (newline)
-		ft_printf("\n");
+		printf("\n");
 	return (0);
 }
 
