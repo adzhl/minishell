be05fd20c8b5a0990d20d79c9100c2083b497245
Ad <103935216+adzhl@users.noreply.github.com>
2024-11-11 08:29:30 +0800

    Modified update_env_value.c to handle cases when ft_strjoin fails

--- src/utils/update_env_value.c
+++ src/utils/update_env_value.c
@@ -6,7 +6,7 @@
 /*   By: abinti-a <abinti-a@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/10 20:01:18 by abinti-a          #+#    #+#             */
-/*   Updated: 2024/11/11 08:15:59 by abinti-a         ###   ########.fr       */
+/*   Updated: 2024/11/11 08:28:23 by abinti-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,15 @@ int	update_env_value(char **env, const char *name, const char *value)
 	char	*temp;
 	char	*new_var;
 
-	len = 0;
-	while (name[len])
-		len++;
+	len = ft_strlen(name);
 	i = 0;
 	while (env[i])
 	{
 		if (ft_strncmp(env[i], name, len) == 0 && env[i][len] == '=')
 		{
 			temp = ft_strjoin(name, "=");
+            if (!temp)
+                return (0);
 			new_var = ft_strjoin(temp, value);
 			free(temp);
 			if (!new_var)
