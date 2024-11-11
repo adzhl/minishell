9b8ae95ca6f8dbadd0f1c7707a1f4c79817e9ddc
Ad <103935216+adzhl@users.noreply.github.com>
2024-11-11 09:15:25 +0800

    Modified builtin_cd.c to update env when changing directory

--- src/builtin/builtin_cd.c
+++ src/builtin/builtin_cd.c
@@ -6,13 +6,16 @@
 /*   By: abinti-a <abinti-a@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 14:57:06 by abinti-a          #+#    #+#             */
-/*   Updated: 2024/11/06 10:54:30 by abinti-a         ###   ########.fr       */
+/*   Updated: 2024/11/11 09:14:32 by abinti-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/builtin.h"
 
-void	print_cd_error(char *arg, char *message)
+/**
+ * Error printing for cd command
+ */
+static void	print_cd_error(char *arg, char *message)
 {
 	ft_putstr_fd("minishell: cd: ", STDERR_FILENO);
 	if (arg)
@@ -23,26 +26,55 @@ void	print_cd_error(char *arg, char *message)
 	ft_putendl_fd(message, STDERR_FILENO);
 }
 
+static int	handle_no_args(char **env, char **path)
+{
+	*path = get_env_value(env, "HOME");
+	if (!*path)
+	{
+		print_cd_error(NULL, "HOME not set");
+		return (1);
+	}
+	return (0);
+}
+
+static int	update_pwd(char **env, char *curr_dir, char *new_dir)
+{
+	if (!getcwd(new_dir, PATH_MAX))
+	{
+		print_cd_error(NULL, "getcwd failed");
+		return (1);
+	}
+	update_env_value(env, "OLDPWD", curr_dir);
+	update_env_value(env, "PWD", new_dir);
+	return (0);
+}
+
+/**
+ * 1. Check for an argument
+ * 2. If no argument, change to HOME env
+ * 3. If there is an argument, use chdir to change to the specified path
+ * 4. If path does not exist, return an error.
+ */
 int	builtin_cd(char **args, char **env)
 {
 	char	*path;
+	char	curr_dir[PATH_MAX];
+	char	new_dir[PATH_MAX];
 
-	(void)env;
-	if (!args[1])
-	{
-		path = getenv("HOME");
-		if (!path)
-		{
-			print_cd_error(NULL, "HOME not set");
-			return (1);
-		}
-	}
-	else
+	(void)args;
+	if (!args[1] && handle_no_args(env, &path))
+		return (1);
+	else if (args[1])
 		path = args[1];
+	if (!getcwd(curr_dir, PATH_MAX))
+	{
+		print_cd_error(NULL, "getcwd failed");
+		return (1);
+	}
 	if (chdir(path) == -1)
 	{
 		print_cd_error(args[1], "No such file or directory");
 		return (1);
 	}
-	return (0);
+	return (update_pwd(env, curr_dir, new_dir));
 }
