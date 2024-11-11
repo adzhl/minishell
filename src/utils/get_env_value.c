fdd8a5b4c0b94906236ce965a36e884c614d04bc
Ad <103935216+adzhl@users.noreply.github.com>
2024-11-11 08:14:12 +0800

    Added utility functions for env (copy_env.c, get_env_value.c, update_env_value.c)

---
+++ src/utils/get_env_value.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_env_value.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abinti-a <abinti-a@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/07 14:53:40 by abinti-a          #+#    #+#             */
+/*   Updated: 2024/11/11 08:06:57 by abinti-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/utils.h"
+
+char *get_env_value(char **env, const char *name)
+{
+    int     i;
+    int     len;
+    
+    if (!name || !env)
+        return (NULL);
+    len = 0;
+    while (name[len])
+        len++;
+    i = 0;
+    while (env[i])
+    {
+        if (ft_strncmp(env[i], name, len) == 0 && env[i][len] == '=')
+            return (env[i] + len + 1);
+        i++;
+    }
+    return (NULL);
+}
