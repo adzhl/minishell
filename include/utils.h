346a2b2aae285b18b5b6a12290e3c74e428bc6ed
Adya Zahila Hasman Binti Aidi Hasman <abinti-a@u90z03s08.42kl.edu.my>
2024-11-12 11:52:31 +0800

    Modified utils.h to comply with norm

--- include/utils.h
+++ include/utils.h
@@ -6,7 +6,7 @@
 /*   By: abinti-a <abinti-a@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 09:27:54 by abinti-a          #+#    #+#             */
-/*   Updated: 2024/11/12 11:31:52 by abinti-a         ###   ########.fr       */
+/*   Updated: 2024/11/12 11:51:47 by abinti-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,14 @@
 # include "../libft/get_next_line.h"
 # include "../libft/libft.h"
 
-char **create_env_copy(char **envp);
-char *get_env_value(char **env, const char *name);
-int update_env_value(char **env, const char *name, const char *value);
+char	**create_env_copy(char **envp);
+char	*get_env_value(char **env, const char *name);
+int		update_env_value(char **env, const char *name, const char *value);
 void	print_exit_error(char *arg, char *message);
-int	find_var_name(char **env, const char *name);
-int	valid_var_name(const char *name);
-void free_array(char **array);
-int print_sorted_env(char **env);
-int	get_env_size(char **env);
+int		find_var_name(char **env, const char *name);
+int		valid_var_name(const char *name);
+void	free_array(char **array);
+int		print_sorted_env(char **env);
+int		get_env_size(char **env);
 
 #endif
