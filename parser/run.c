ac35dbc0f3245713f5de3707a40076cb337a45f2
Eu Ting Tien <etien@u82z03s01.42kl.edu.my>
2024-11-14 18:33:33 +0800

    created function to pipe heredoc. redirection is fully working. also fixed key logic in word tokenizer so that quoted strings without spaces between them are still considered one argument.

--- parser/run.c
+++ parser/run.c
@@ -6,7 +6,7 @@
 /*   By: etien <etien@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 11:51:24 by etien             #+#    #+#             */
-/*   Updated: 2024/11/14 15:05:21 by etien            ###   ########.fr       */
+/*   Updated: 2024/11/14 16:18:31 by etien            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,26 +27,18 @@ void	run_cmd(t_cmd *cmd)
 	if (!cmd)
 		exit(EXIT_FAILURE);
 	if (cmd->type == PIPE)
-	{
-		set_pipes((t_pipe_cmd *)cmd);
-		printf("pipes set\n");
-	}
+		set_pipe((t_pipe_cmd *)cmd);
 	else if (cmd->type == EXEC)
 	{
 		ecmd = (t_exec_cmd *)cmd;
 		if (ecmd->argv[0] == 0)
 			exit(1);
-		printf("execution happens here\n");
 		execve(ecmd->argv[0], ecmd->argv, environ);
 		ft_putstr_fd(EXEC_ERR, 2);
 		ft_putendl_fd(ecmd->argv[0], 2);
 	}
 	else if (cmd->type == REDIR)
-	{
-		open_fd((t_redir_cmd *)cmd);
-		printf("redir of %s\n", ((t_redir_cmd *)cmd)->file);
-		run_cmd(((t_redir_cmd *)cmd)->cmd);
-	}
+		set_redirection((t_redir_cmd *)cmd);
 	exit(EXIT_SUCCESS);
 }
 
@@ -56,7 +48,7 @@ void	run_cmd(t_cmd *cmd)
 // The parent process will wait for both children to terminate.
 // Even though fork is called twice, only two child processes are created
 // because each child will exit when run_cmd is called.
-void	set_pipes(t_pipe_cmd	*pcmd)
+void	set_pipe(t_pipe_cmd	*pcmd)
 {
 	int	pipefd[2];
 
@@ -82,31 +74,22 @@ void	set_pipes(t_pipe_cmd	*pcmd)
 	wait(NULL);
 }
 
-// This is a simple helper function to close both ends of a pipe.
-void	close_pipes(int *pipefd)
+// This function will set up the correct redirection depending
+// on whether it is a file or a heredoc. In both cases, it will
+// call run_cmd to continue descending down the parsing tree.
+void	set_redirection(t_redir_cmd *rcmd)
 {
-	close(pipefd[READ]);
-	close(pipefd[WRITE]);
-}
-
-// This is a wrapper for the fork function.
-// It will throw an error if the fork fails. Otherwise, it
-// will return the child process pid.
-int	fork_and_check(void)
-{
-	int	pid;
-
-	pid = fork();
-	if (pid < 0)
-		perror(FORK_ERR);
-	return (pid);
+	if (rcmd->file)
+		open_fd(rcmd);
+	else if (rcmd->heredoc)
+		pipe_heredoc(rcmd);
+	run_cmd(rcmd->cmd);
 }
 
 // This function handles opening file descriptors for the REDIR nodes.
 // If the third parameter in open() is omitted, file permissions default to
-// the shell's umask (typically 022, which results in 0644 permissions).
-// To be able to open the newly-created file, we set the file permissions
-// to the more generous 0666.
+// the shell's umask (typically 022, which results in 0644 permissions). The
+// more generous 0666 permissions will allow us to open the newly-created file.
 // In a sequence of REDIR nodes, later redirections overwrite earlier ones
 // because dup2 continuously reconfigures STDIN or STDOUT as we traverse
 // down the chain.
@@ -122,4 +105,26 @@ void	open_fd(t_redir_cmd *rcmd)
 		exit(1);
 	}
 	dup2(redir_fd, rcmd->fd);
+}
+
+// This function will write the heredoc to a pipe so that it will
+// have a file descriptor that can interact with the dup2 function.
+// Since heredocs are typically temporary files, the heredoc string
+// can be immediately freed after it has been written to the pipe for
+// memory efficiency.
+void	pipe_heredoc(t_redir_cmd *rcmd)
+{
+	int	pipefd[2];
+
+	if (pipe(pipefd) < 0)
+	{
+		perror(PIPE_ERR);
+		exit(EXIT_FAILURE);
+	}
+	write(pipefd[WRITE], rcmd->heredoc, ft_strlen(rcmd->heredoc));
+	close(pipefd[WRITE]);
+	dup2(pipefd[READ], STDIN_FILENO);
+	close(pipefd[READ]);
+	free(rcmd->heredoc);
+	rcmd->heredoc = NULL;
 }
