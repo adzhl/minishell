6c37874a14e1f2431ec3d84657ca7aeb5c2c4a3d
et-learns-to-code <155518416+et-learns-to-code@users.noreply.github.com>
2024-11-15 12:09:49 +0800

    created basic AST freeing function. fix norm later.

--- parser/clean_up.c
+++ parser/clean_up.c
@@ -6,10 +6,59 @@
 /*   By: etien <etien@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 14:01:00 by etien             #+#    #+#             */
-/*   Updated: 2024/11/14 14:02:54 by etien            ###   ########.fr       */
+/*   Updated: 2024/11/15 12:02:46 by etien            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-// free_
+// This clean-up function will free the abstract syntax tree.
+// It will traverse down the tree recursively and free children nodes
+// first before freeing the parent nodes.
+void	free_ast(t_cmd *cmd)
+{
+	t_pipe_cmd	*pcmd;
+	t_redir_cmd	*rcmd;
+	t_exec_cmd	*ecmd;
+
+	cmd_typecasting(cmd, &pcmd, &rcmd, &ecmd);
+	if (!cmd)
+		return ;
+	if (cmd->type == PIPE)
+	{
+		free_ast(pcmd->left);
+		free_ast(pcmd->right);
+		free(pcmd);
+	}
+	else if (cmd->type == REDIR)
+	{
+		free_ast(rcmd->cmd);
+		free_redir_data(rcmd);
+		free(rcmd);
+	}
+	else if (cmd->type == EXEC)
+	{
+		free_exec_data(ecmd);
+		free(ecmd);
+	}
+}
+
+// A helper function to free the redir fields.
+void	free_redir_data(t_redir_cmd *rcmd)
+{
+	if (rcmd->file)
+		free(rcmd->file);
+	if (rcmd->heredoc)
+		free(rcmd->heredoc);
+}
+
+// A helper function to free the
+void	free_exec_data(t_exec_cmd *ecmd)
+{
+	int	i;
+
+	i = -1;
+	if (ecmd->argv)
+		while (ecmd->argv[++i])
+			free(ecmd->argv[i]);
+}
