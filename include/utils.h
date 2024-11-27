17c9e8eb4d5d4da01b54e4640ecbdfca2843609c
Adya Zahila Hasman Binti Aidi Hasman <abinti-a@u90z05s03.42kl.edu.my>
2024-11-27 09:09:17 +0800

    Modified header files and Makefiles to include the correct libraries and files

--- include/utils.h
+++ include/utils.h
@@ -6,17 +6,19 @@
 /*   By: abinti-a <abinti-a@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 09:27:54 by abinti-a          #+#    #+#             */
-/*   Updated: 2024/11/27 08:56:16 by abinti-a         ###   ########.fr       */
+/*   Updated: 2024/11/27 09:07:17 by abinti-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef UTILS_H
 # define UTILS_H
 
+# include "../libft/libft/libft.h"
+# include <stdio.h>
+
 char	**create_env_copy(char **envp);
 char	*get_env_value(char **env, const char *name);
 int		update_env_value(char **env, const char *name, const char *value);
-void	print_exit_error(char *arg, char *message);
 int		find_var_name(char **env, const char *name);
 int		valid_var_name(const char *name);
 void	free_array(char **array);
