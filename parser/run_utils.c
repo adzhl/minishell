3978769788809fc44c896e370e88e5941c860978
Eu Ting Tien <etien@u81z09s02.42kl.edu.my>
2024-11-28 17:30:23 +0800

    fixed run_cmd_control to run standalone built-in commands in parent process.

--- parser/run_utils.c
+++ parser/run_utils.c
@@ -3,31 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   run_utils.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abinti-a <abinti-a@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*   By: etien <etien@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 16:10:16 by etien             #+#    #+#             */
-/*   Updated: 2024/11/27 13:38:38 by abinti-a         ###   ########.fr       */
+/*   Updated: 2024/11/28 17:21:29 by etien            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-// This function will detect for pipe symbols in the input and either
-// call run_cmd directly (if there are pipes) or fork a child process
-// to run simple commands. run_cmd will fork child processes for PIPE
-// nodes, but we will have to fork them manually if the input does not
-// contain pipes. This is necessary so that control will be returned to
-// the parent process after the child executes the command and terminates.
-// It will prevent the program from exiting immediately.
+char	*get_standalone_cmd(t_cmd *ast);
+void	run_built_in(t_cmd *cmd, t_mshell *shell);
+
+// This function checks for pipe symbols in the input to differentiate
+// between standalone commands and piped commands. For piped commands,
+// the run_cmd function can be called directly, as it handles child process
+// creation in its PIPE nodes. However, for standalone commands, built-in
+// functions must be executed in the parent process to ensure that changes
+// to the environment variables (ENV) persist across subsequent commands.
+// If the standalone command is not a built-in, a child process must be forked
+// manually to execute it. This prevents the execve call from terminating
+// the parent process. The goal is to ensure that control returns to the
+// shell prompt after execution, avoiding an immediate program exit.
 void	run_cmd_control(char *input, t_cmd *ast, t_mshell *shell)
 {
 	bool	has_pipe;
 
 	has_pipe = ft_strchr(input, '|');
 	free(input);
-	if (has_pipe)
-		run_cmd(ast, shell);
-	else
+	if (!has_pipe && !is_builtin(get_standalone_cmd(ast)))
 	{
 		if (fork() == 0)
 		{
@@ -36,6 +40,47 @@ void	run_cmd_control(char *input, t_cmd *ast, t_mshell *shell)
 		}
 		wait(NULL);
 	}
+	else if (!has_pipe && is_builtin(get_standalone_cmd(ast)))
+		run_built_in(ast, shell);
+	else
+		run_cmd(ast, shell);
+}
+
+// This function is similar to the run_cmd function, except it
+// is only run when there is a standalone built-in command.
+void	run_built_in(t_cmd *cmd, t_mshell *shell)
+{
+	t_redir_cmd	*rcmd;
+	t_exec_cmd	*ecmd;
+
+	cmd_typecasting(cmd, NULL, &rcmd, &ecmd);
+	if (!cmd)
+		return ;
+	else if (cmd->type == EXEC)
+	{
+		if (ecmd->argv[0] == 0)
+			return ;
+		if (is_builtin(ecmd->argv[0]))
+		{
+			if (execute_builtin(ecmd->argv[0], ecmd->argv, shell) != 0)
+				return ;
+		}
+	}
+	else if (cmd->type == REDIR)
+		set_redirection(rcmd, shell);
+}
+
+// This function will fetch the name of the standalone command.
+char	*get_standalone_cmd(t_cmd *ast)
+{
+	t_cmd	*current_node;
+
+	current_node = ast;
+	while (current_node->type == REDIR)
+		current_node = ((t_redir_cmd *)current_node)->cmd;
+	if (current_node->type == EXEC)
+		return (((t_exec_cmd *)current_node)->argv[0]);
+	return (NULL);
 }
 
 // This is a helper function to close both ends of a pipe.
@@ -49,7 +94,10 @@ void	close_pipes(int *pipefd)
 void	cmd_typecasting(t_cmd *cmd,
 	t_pipe_cmd **pcmd, t_redir_cmd **rcmd, t_exec_cmd **ecmd)
 {
-	*pcmd = (t_pipe_cmd *)cmd;
-	*rcmd = (t_redir_cmd *)cmd;
-	*ecmd = (t_exec_cmd *)cmd;
+	if (pcmd)
+		*pcmd = (t_pipe_cmd *)cmd;
+	if (rcmd)
+		*rcmd = (t_redir_cmd *)cmd;
+	if (ecmd)
+		*ecmd = (t_exec_cmd *)cmd;
 }
