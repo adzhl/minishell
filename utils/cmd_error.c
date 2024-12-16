9ed470f075b35a630d618298e47c7a0622a9d6be
Adya Zahila Hasman Binti Aidi Hasman <abinti-a@u90z05s03.42kl.edu.my>
2024-12-16 16:31:02 +0800

    1. minishell.h: Modified error message for EXEC_ERR and NO_ACCESS 2. cmd_error(): Modified error message sequence to 'msh: cmd: error message'

--- utils/cmd_error.c
+++ utils/cmd_error.c
@@ -6,7 +6,7 @@
 /*   By: abinti-a <abinti-a@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/04 18:29:57 by abinti-a          #+#    #+#             */
-/*   Updated: 2024/12/16 09:34:29 by abinti-a         ###   ########.fr       */
+/*   Updated: 2024/12/16 16:25:44 by abinti-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,15 +22,17 @@ void	cmd_error(char *cmd_path, t_exec_cmd *ecmd, int mode)
 	{
 		if (access(ecmd->argv[0], F_OK) == 0)
 		{
-			ft_putstr_fd(NO_ACCESS, STDERR_FILENO);
-			ft_putendl_fd(ecmd->argv[0], STDERR_FILENO);
+			ft_putstr_fd("msh: ", STDERR_FILENO);
+			ft_putstr_fd(ecmd->argv[0], STDERR_FILENO);
+			ft_putendl_fd(NO_ACCESS, STDERR_FILENO);
 			exit(126);
 		}
 	}
 	if (cmd_path)
 		free(cmd_path);
-	ft_putstr_fd(EXEC_ERR, STDERR_FILENO);
-	ft_putendl_fd(ecmd->argv[0], STDERR_FILENO);
+	ft_putstr_fd("msh: ", STDERR_FILENO);
+	ft_putstr_fd(ecmd->argv[0], STDERR_FILENO);
+	ft_putendl_fd(EXEC_ERR, STDERR_FILENO);
 	if (mode)
 		exit(EXIT_FAILURE);
 	exit(127);
